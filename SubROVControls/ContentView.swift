91b49d176d0b0dfb10aa7d6f9d7c29138fa0b008
Matthew Armstrong <armstrongmg1@gmail.com>
2024-02-23 18:34:03 -0800

    initial commit

---
+++ SubROVControls/ContentView.swift
@@ -0,0 +1,105 @@
+//
+//  ContentView.swift
+//  SubROVControls
+//
+//  Created by Matthew Armstrong on 2/23/24.
+//
+
+import SwiftUI
+import CoreBluetooth
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [.black, .black],
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)       .edgesIgnoringSafeArea(.all)
+            VStack {
+                // Forward Button
+                Button {
+                    print("Action to move forward here")
+                } label: {
+                    Text("FORWARD")
+                        .foregroundColor(.black)
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                        .frame(width: 200, height: 200)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        .font(.title)
+                }
+                .buttonStyle(.borderedProminent)
+                .controlSize(.large)
+                .buttonBorderShape(.capsule)
+                .tint(Color(red: 98/255, green: 255/255, blue: 152/255))
+                .padding([.bottom, .trailing, .top], 50)
+                .rotationEffect(.degrees(90))
+                
+                HStack {
+                    VStack {
+                        // Left button
+                        Button {
+                            print("Action to move left here")
+                        } label: {
+                            Image(systemName: "arrowshape.left.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundStyle(.black)
+                                .frame(width: 75, height: 75)
+                                .clipShape(Rectangle())
+                        }
+                        .controlSize(.large)
+                        .buttonStyle(.borderedProminent)
+                        .buttonBorderShape(.roundedRectangle)
+                        .padding(.trailing, 100)
+                        .tint(Color(red: 237/255, green: 51/255, blue: 78/255))
+                        .rotationEffect(.degrees(90))
+
+                        // Right button
+                        Button {
+                            print("Action to move right here")
+                        } label: {
+                            Image(systemName: "arrowshape.right.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundStyle(.black)
+                                .frame(width: 75, height: 75)
+                                .clipShape(Rectangle())
+                        }
+                        .controlSize(.large)
+                        .buttonStyle(.borderedProminent)
+                        .buttonBorderShape(.roundedRectangle)
+                        .tint(Color(red: 237/255, green: 51/255, blue: 78/255))
+                        .rotationEffect(.degrees(90))
+
+                    }
+                    .padding([.top], 50)
+
+                    // Trash Collector Button
+                    Button {
+                        print("Activate Trash Collector")
+                    } label: {
+                        Image(systemName: "trash.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundStyle(.black)
+                            .frame(maxWidth: .infinity)
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                    }
+                    .controlSize(.large)
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle)
+                    .padding([.top], 75)
+                    .tint(Color(red: 92/255, green: 189/255, blue: 235/255))
+                    .rotationEffect(.degrees(90))
+
+                }
+            }
+
+        }
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SubROVControls/SubROVControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubROVControlsApp.swift
+//  SubROVControls
+//
+//  Created by Matthew Armstrong on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SubROVControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
